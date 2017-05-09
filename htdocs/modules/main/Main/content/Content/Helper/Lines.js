36e39e65574ac94b722cca1cd1eb81aed1c5d4eb
micty <79230533@qq.com>
2017-05-09 16:29:49 +0800

    fold

--- htdocs/modules/main/Main/content/Content/Helper/Lines.js
+++ htdocs/modules/main/Main/content/Content/Helper/Lines.js
@@ -6,7 +6,10 @@ define('/Main/Content/Helper/Lines', function (require, module, exports) {
     var $ = require('$');
     var MiniQuery = require('MiniQuery');
     
-    return {
+
+    var sample = '<ul class="line-numbers" style="height: {height}px">{lis}</ul>';
+
+    return exports = {
 
         //产生行号的 html
         'getNumbers': function (code) {
@@ -17,14 +20,21 @@ define('/Main/Content/Helper/Lines', function (require, module, exports) {
                 return '<li>' + (index + 1) + '</li>';
             });
 
-            var html = '<ul class="line-numbers">' + lines.join('') + '</ul>';
+            var height = exports.getHeight(lines);
+
+            var html = $.String.format(sample, {
+                'height': height,
+                'lis': lines.join(''),
+            });
 
             return html;
         },
 
         //根据文本内容计算需要的高度。
         'getHeight': function (code) {
-            var lines = code.split(/\r\n|\n|\r/);
+            var lines = Array.isArray(code) ? code :
+                code.split(/\r\n|\n|\r/);
+
             return lines.length * 20;
         },
     };
